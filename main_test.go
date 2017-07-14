457f5ee94d57eb5438827e3379d8853bf2ef767a
Joshua Sandlin <josh.sandlin@turner.com>
2017-07-14 04:15:56 -0400

    initial commit

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "testing"
+)
+
+func TestConfif (t *testing.T) {
+    contentBytes, err := ioutil.ReadFile("config.json")
+
+    if err != nil {
+        t.Errorf("The config file is missing")
+    }
+
+    var c config
+
+    if err = json.Unmarshal(contentBytes, &c); err != nil {
+        t.Errorf("Error while parsing the config file")
+    }
+
+    if c.Greeting == "" {
+        t.Errorf("The config file is missing the Greet key")
+    }
+}
